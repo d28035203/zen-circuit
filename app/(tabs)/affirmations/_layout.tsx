32df72da191f7bd2c9176274670abc8fb58b246a
d28035203 <289484834+d28035203@users.noreply.github.com>
2025-05-06 13:20:37 +0530

    feat: implement Expo Router screens and tabs

---
+++ app/(tabs)/affirmations/_layout.tsx
@@ -0,0 +1,28 @@
+/**
+ * _layout.tsx — animated-enigma
+ * @author d28035203
+ */
+
+import React from "react";
+import { Stack } from "expo-router";
+
+const AffirmationsLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="[itemId]"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default AffirmationsLayout;
